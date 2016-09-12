be27e01732903a36de9939b6e2922332fa83d7ba
Francis Campbell <franciscampbel@hotmail.com>
2016-09-12 15:06:22 -0400

    delete csm-env

--- csm-env/bin/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!/home/francis/Bureau/Django-Ubuntu/sites_dev/csm-w/csm-env/bin/python3
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
